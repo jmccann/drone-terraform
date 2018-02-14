0faae5eb94364d16f7c51e3b09c9ae4f5d1352f6
Jacob McCann <jacob.mccann2@target.com>
2018-02-14 09:39:20 -0600

    Alphabetize flags

--- main.go
+++ main.go
@@ -23,30 +23,30 @@ func main() {
 		// plugin args
 		//
 
-		cli.StringFlag{
-			Name:   "init_options",
-			Usage:  "options for the init command. See https://www.terraform.io/docs/commands/init.html",
-			EnvVar: "PLUGIN_INIT_OPTIONS",
-		},
-		cli.StringFlag{
-			Name:   "vars",
-			Usage:  "a map of variables to pass to the Terraform `plan` and `apply` commands. Each value is passed as a `<key>=<value>` option",
-			EnvVar: "PLUGIN_VARS",
-		},
-		cli.StringFlag{
-			Name:   "secrets",
-			Usage:  "a map of secrets to pass to the Terraform `plan` and `apply` commands. Each value is passed as a `<key>=<ENV>` option",
-			EnvVar: "PLUGIN_SECRETS",
+		cli.StringSliceFlag{
+			Name:   "actions",
+			Usage:  "a list of actions to have terraform perform",
+			EnvVar: "PLUGIN_ACTIONS",
+			Value:  &cli.StringSlice{"validate", "apply"},
 		},
 		cli.StringFlag{
 			Name:   "ca_cert",
 			Usage:  "ca cert to add to your environment to allow terraform to use internal/private resources",
 			EnvVar: "PLUGIN_CA_CERT",
 		},
-		cli.BoolFlag{
-			Name:   "sensitive",
-			Usage:  "whether or not to suppress terraform commands to stdout",
-			EnvVar: "PLUGIN_SENSITIVE",
+		cli.StringFlag{
+			Name:  "env-file",
+			Usage: "source env file",
+		},
+		cli.StringFlag{
+			Name:   "init_options",
+			Usage:  "options for the init command. See https://www.terraform.io/docs/commands/init.html",
+			EnvVar: "PLUGIN_INIT_OPTIONS",
+		},
+		cli.IntFlag{
+			Name:   "parallelism",
+			Usage:  "The number of concurrent operations as Terraform walks its graph",
+			EnvVar: "PLUGIN_PARALLELISM",
 		},
 		cli.StringFlag{
 			Name:   "role_arn_to_assume",
@@ -58,38 +58,35 @@ func main() {
 			Usage:  "The root directory where the terraform files live. When unset, the top level directory will be assumed",
 			EnvVar: "PLUGIN_ROOT_DIR",
 		},
-		cli.IntFlag{
-			Name:   "parallelism",
-			Usage:  "The number of concurrent operations as Terraform walks its graph",
-			EnvVar: "PLUGIN_PARALLELISM",
-		},
-
 		cli.StringFlag{
-			Name:  "env-file",
-			Usage: "source env file",
+			Name:   "secrets",
+			Usage:  "a map of secrets to pass to the Terraform `plan` and `apply` commands. Each value is passed as a `<key>=<ENV>` option",
+			EnvVar: "PLUGIN_SECRETS",
 		},
-
+		cli.BoolFlag{
+			Name:   "sensitive",
+			Usage:  "whether or not to suppress terraform commands to stdout",
+			EnvVar: "PLUGIN_SENSITIVE",
+		},
 		cli.StringSliceFlag{
 			Name:   "targets",
 			Usage:  "targets to run apply or plan on",
 			EnvVar: "PLUGIN_TARGETS",
-		},
-
-		cli.StringSliceFlag{
-			Name:   "var_files",
-			Usage:  "a list of var files to use. Each value is passed as -var-file=<value>",
-			EnvVar: "PLUGIN_VAR_FILES",
 		},
 		cli.StringFlag{
 			Name:   "tf.version",
 			Usage:  "terraform version to use",
 			EnvVar: "PLUGIN_TF_VERSION",
 		},
+		cli.StringFlag{
+			Name:   "vars",
+			Usage:  "a map of variables to pass to the Terraform `plan` and `apply` commands. Each value is passed as a `<key>=<value>` option",
+			EnvVar: "PLUGIN_VARS",
+		},
 		cli.StringSliceFlag{
-			Name:   "actions",
-			Usage:  "a list of actions to have terraform perform",
-			EnvVar: "PLUGIN_ACTIONS",
-			Value:  &cli.StringSlice{"validate", "apply"},
+			Name:   "var_files",
+			Usage:  "a list of var files to use. Each value is passed as -var-file=<value>",
+			EnvVar: "PLUGIN_VAR_FILES",
 		},
 	}
 
