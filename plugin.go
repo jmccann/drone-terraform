0365dccf992b24d999502a983b3ff96493b71430
Jeff Hung <jhung@ruckuswireless.com>
2018-06-07 11:24:21 -0700

    Fix incorrect format for terraform validate -var-file command

--- plugin.go
+++ plugin.go
@@ -312,7 +312,7 @@ func tfValidate(config Config) *exec.Cmd {
 		"validate",
 	}
 	for _, v := range config.VarFiles {
-		args = append(args, "-var-file=%s", fmt.Sprintf("%s", v))
+		args = append(args, fmt.Sprintf("-var-file=%s", v))
 	}
 	for k, v := range config.Vars {
 		args = append(args, "-var")
