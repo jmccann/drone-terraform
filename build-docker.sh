0cea4e7cfcbdb85376b0658ca6b8615ccb3f4d6a
Jacob McCann <jacob.mccann2@target.com>
2020-02-05 10:53:27 -0600

    feat: terraform 0.12.20

--- build-docker.sh
+++ build-docker.sh
@@ -12,7 +12,7 @@ fi
 major=$(echo $tag | awk -F. '{print $1}')
 minor=$(echo $tag | awk -F. '{print $2}')
 
-tf_ver="0.12.16"
+tf_ver="0.12.20"
 
 echo "Confirm building images for:"
 echo "  MAJOR: ${major}"
