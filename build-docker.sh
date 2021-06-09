52d681822a7da9ff799115b6ab478b7e1b44a7b8
Joachim Hill-Grannec <joachim.hill@gmail.com>
2021-06-09 11:38:20 -0400

    update to terraform 1.0.0

--- build-docker.sh
+++ build-docker.sh
@@ -12,7 +12,7 @@ fi
 major=$(echo $tag | awk -F. '{print $1}')
 minor=$(echo $tag | awk -F. '{print $2}')
 
-tf_ver="0.15.1"
+tf_ver="1.0.0"
 
 echo "Confirm building images for:"
 echo "  MAJOR: ${major}"
